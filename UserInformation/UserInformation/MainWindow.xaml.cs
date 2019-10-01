4b34fd089068487a5ff1d0a365bee769ce5de377
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-10-01 15:24:12 -0500

    Made Early Draft of Window Form

--- UserInformation/UserInformation/MainWindow.xaml.cs
+++ UserInformation/UserInformation/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using DatabaseObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,42 @@ namespace UserInformation
         public MainWindow()
         {
             InitializeComponent();
+            Insert.Visibility=Visibility.Hidden;
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            user.FirstName = First.Text;
+            user.LastName = Last.Text;
+            user.State = State.Text;
+            user.Zip = Convert.ToInt32(Zip.Text);
+            user.City = City.Text;
+            user.DOB = Dob.SelectedDate.Value;
+
+
+            BusinessLogic.InsertUser(user);
+
+            List<User> getUsers = BusinessLogic.GetUsers();
+            User check = getUsers.Find(m => m.FirstName == user.FirstName && m.LastName == user.LastName);
+
+            if(check.FirstName != null)
+            {
+                
+                Insert.Content = "User Added";
+                Insert.Visibility = Visibility.Visible;
+                Clear();
+                
+            }
+            
+        }
+        public void Clear()
+        {
+            First.Text = "";
+            Last.Text = "";
+            State.Text = "";
+            Zip.Text = "";
+            City.Text = "";
         }
     }
 }
